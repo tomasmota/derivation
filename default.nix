7e4cc3b58b0ce80e55028bb286e6fa9a06223117
tomasmota <tomasrebelomota@gmail.com>
2023-05-29 21:35:11 +0200

    tmp
    Signed-off-by: tomasmota <tomasrebelomota@gmail.com>

--- default.nix
+++ default.nix
@@ -1,13 +1,16 @@
 let
   nixpkgs = import <nixpkgs> {};
   allPkgs = nixpkgs // packages;
-  callPackage = path: overrides:
+  lib = import ./lib.nix;
+  callPackageBase = path: overrides:
     let f = import path;
     in f ((builtins.intersectAttrs (builtins.functionArgs f) allPkgs) // overrides);
+  callPackage = path: lib.makeOverridable (callPackageBase path);
   packages = with nixpkgs; {
     mkDerivation = import ./autotools.nix nixpkgs;
-    hello = callPackage ./hello.nix { };
+    # hello = callPackage ./hello.nix { };
     graphviz = callPackage ./graphviz.nix { };
-    graphvizcore = callPackage ./graphviz.nix { gdSupport = false; };
+    # graphvizcore = callPackage ./graphviz.nix { gdSupport = false; };
+    graphvizcore = graphviz.override { gdSupport = false; }; 
 };
 in packages
