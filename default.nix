4a19dec3936af59d7800bb676c6ccd1c19d939c9
Tomás Mota <tmm@danskecommodities.com>
2023-05-30 07:48:20 +0200

    fix hello
    Signed-off-by: Tomás Mota <tmm@danskecommodities.com>

--- default.nix
+++ default.nix
@@ -2,15 +2,15 @@ let
   nixpkgs = import <nixpkgs> {};
   allPkgs = nixpkgs // packages;
   lib = import ./lib.nix;
-  callPackageBase = path: overrides:
+  callPackage = path: overrides:
     let f = import path;
     in f ((builtins.intersectAttrs (builtins.functionArgs f) allPkgs) // overrides);
-  callPackage = path: lib.makeOverridable (callPackageBase path);
+  # callPackage = path: lib.makeOverridable (callPackageBase path);
   packages = with nixpkgs; {
     mkDerivation = import ./autotools.nix nixpkgs;
-    # hello = callPackage ./hello.nix { };
+    hello = callPackage ./hello.nix { };
     graphviz = callPackage ./graphviz.nix { };
-    # graphvizcore = callPackage ./graphviz.nix { gdSupport = false; };
-    graphvizcore = graphviz.override { gdSupport = false; }; 
+    graphvizcore = callPackage ./graphviz.nix { gdSupport = false; };
+    # graphvizcore = graphviz.override { gdSupport = false; }; 
 };
 in packages
--- hello.nix
+++ hello.nix
@@ -1,8 +1,6 @@
-let 
-  pkgs = import <nixpkgs> {};
-  mkDerivation = import ./autotools.nix pkgs;
-in mkDerivation {
+{ mkDerivation }:
+
+mkDerivation {
   name = "hello-git";
   src = ./hello-2.10.tar.gz;
-  buildInputs = [ pkgs.gdb ];
 }
