86f497cfc2cb9096dd203c432b144834f8588e8c
tomasmota <tomasrebelomota@gmail.com>
2023-06-05 14:41:20 +0200

    use stdenv mkDerivation
    Signed-off-by: tomasmota <tomasrebelomota@gmail.com>

--- default.nix
+++ default.nix
@@ -1,13 +1,13 @@
 let
   nixpkgs = import <nixpkgs> {};
-  allPkgs = nixpkgs // packages;
+  allPkgs = nixpkgs // pkgs;
   callPackage = path: overrides:
     let f = import path;
     in f ((builtins.intersectAttrs (builtins.functionArgs f) allPkgs) // overrides);
-  packages = with nixpkgs; {
+  pkgs = with nixpkgs; {
     mkDerivation = import ./autotools.nix nixpkgs;
     hello = callPackage ./hello.nix { };
     graphviz = callPackage ./graphviz.nix { };
-    graphvizcore = callPackage ./graphviz.nix { gdSupport = false; };
-};
-in packages
+    graphvizCore = callPackage ./graphviz.nix { gdSupport = false; };
+  };
+in pkgs
--- hello.nix
+++ hello.nix
@@ -1,8 +1,14 @@
-let 
-  pkgs = import <nixpkgs> {};
-  mkDerivation = import ./autotools.nix pkgs;
-in mkDerivation {
-  name = "hello-git";
+# let 
+#   pkgs = import <nixpkgs> {};
+#   mkDerivation = import ./autotools.nix pkgs;
+# in mkDerivation {
+#   name = "hello-git";
+#   src = ./hello-2.10.tar.gz;
+#   buildInputs = [ pkgs.gdb ];
+# }
+
+with import <nixpkgs> {};
+stdenv.mkDerivation {
+  name = "hello";
   src = ./hello-2.10.tar.gz;
-  buildInputs = [ pkgs.gdb ];
 }
